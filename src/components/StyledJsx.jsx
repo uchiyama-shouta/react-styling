b7ce2f4b371eb9be0616f4f85cb46c00176b1d61
utiyama shouta <67624765+uchiyama-shouta@users.noreply.github.com>
2021-03-08 03:48:44 +0900

    Initial commit

---
+++ src/components/StyledJsx.jsx
@@ -0,0 +1,36 @@
+export const StyledJsx = () => {
+  return (
+    <>
+      <div className="conteiner">
+        <p className="title">- styled-jsx -</p>
+        <button className="button">Fight!!!</button>
+      </div>
+      <style jsx={true}>{`
+        .conteiner {
+          border: solid 2px #392eff;
+          border-radius: 20px;
+          padding: 8px;
+          margin: 8px;
+          display: flex;
+          justify-content: space-around;
+          align-items: center;
+        }
+        .title {
+          margin: 0;
+          color: #3d84a8;
+        }
+        .button {
+          background-color: #abedd8;
+          border: none;
+          padding: 8px;
+          border-radius: 8px;
+          &:hover {
+            background-color: #46cdcf;
+            color: #fff;
+            cursor: pointer;
+          }
+        }
+      `}</style>
+    </>
+  );
+};
